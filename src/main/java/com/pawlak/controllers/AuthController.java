92cc3ff7fa8f572c0c703cc0540c2330a90f325e
zvit3k <witold.pawlak@interia.pl>
2017-08-01 20:02:30 +0200

    Update school added

--- src/main/java/com/pawlak/controllers/AuthController.java
+++ src/main/java/com/pawlak/controllers/AuthController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 class AuthController {
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loadAuthPage() {
 		return "login";
 	}
+
 }
--- src/main/java/com/pawlak/controllers/SchoolController.java
+++ src/main/java/com/pawlak/controllers/SchoolController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.pawlak.classes.School;
 import com.pawlak.classes.User;
-import com.pawlak.repositories.SchoolRepository;
 import com.pawlak.service.SchoolService;
 import com.pawlak.service.UserService;
 
@@ -44,7 +43,7 @@ public class SchoolController {
 
 		return "redirect:/profile";
 	}
-
+	
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteSchool(@PathVariable Long id, Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -56,6 +55,32 @@ public class SchoolController {
 		userService.updateUser(user);
 		schoolService.deleteSchool(school);
 
+		return "redirect:/profile";
+	}
+	
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
+	public String getUpdateSchool(@PathVariable Long id, Model model) {
+		/*Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String currentPrincipalName = authentication.getName();
+		User user = userService.findByUsername(currentPrincipalName);*/
+		School school = schoolService.getSchoolById(id);
+		model.addAttribute("school", school);
+		return "users/school/update";
+	}
+	
+	@RequestMapping(value = "/updateS", method = RequestMethod.POST)
+	public String sentUpdateSchool(@ModelAttribute School school, BindingResult result, Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String currentPrincipalName = authentication.getName();
+		User user = userService.findByUsername(currentPrincipalName);
+		
+		School s = user.getSchool();
+		s.setName(school.getName());
+		s.setDescription(school.getDescription());
+		s.setPrice(school.getPrice());
+		s.setNumberOfHours(school.getNumberOfHours());
+		schoolService.updateSchool(s);
+		
 		return "redirect:/profile";
 	}
 }
